12fdce3355660931bafb226433b686736b0d14cf
CONG878 <98112875+CONG878@users.noreply.github.com>
2024-09-30 15:34:45 +0900

    details.js 비동기 구조 변경
    상품 정보 data/products.json을 불러오는 fetch.then 함수를 async, await 구문으로 변경.
    header, footer, modal은 fetch.then 구조 유지.

--- sang_il_moon/js/details.js
+++ sang_il_moon/js/details.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const productId = getProductIdFromURL();  // URL에서 id 값을 가져옴
-
     // Load header
     fetch('header.html')
         .then(response => response.text())
@@ -22,49 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
             document.querySelector('dialog').outerHTML = data;
         });
 
-    // JSON에서 상품 ID로 특정 상품 불러오기
-    fetch('data/products.json')  // JSON 파일 경로
-        .then(response => response.json())
-        .then(products => {
-            const product = products.find(p => p.id === parseInt(productId)); // 해당 id의 상품 찾기
-
-            if (!product) {
-                console.error('해당 상품을 찾을 수 없습니다.');
-                return;
-            }
-
-            // 1. 제품명 표시
-            document.querySelector('figcaption h1').textContent = product.productName;
-
-            // 2. 스펙 표시
-            const dl = document.querySelector('figcaption dl');
-            // dl.innerHTML = ''; //초기화 필요시 추가
-            for (let [specName, specValue] of Object.entries(product.specs)) {
-                const span = document.createElement('span');
-                span.innerHTML = `<dt>${specName}:</dt> <dd>${specValue}</dd>`;
-                dl.appendChild(span);
-            }
-
-            // 3. 상품 설명 표시
-            document.querySelector('.product-description span').textContent = product.description;
-
-            // 4. 의견 표시
-            const opinionsSection = document.querySelector('.product-opinions');
-            // opinionsSection.innerHTML = ''; //초기화 필요시 추가
-            product.opinions.forEach(opinion => {
-                const article = document.createElement('article');
-                article.classList.add('opinion');
-                article.innerHTML = `
-                    <p>${opinion.content}</p>
-                    <small class="opinion-meta">
-                        작성자: ${opinion.author}
-                        <time datetime="${opinion.date}">${formatDate(opinion.date)}</time>
-                    </small>
-                `;
-                opinionsSection.appendChild(article);
-            });
-        })
-        .catch(error => console.error('데이터를 불러오는 중 오류 발생:', error));
+    loadProductData();
 });
 
 // 날짜 포맷 함수
@@ -77,4 +33,53 @@ function formatDate(dateString) {
 function getProductIdFromURL() {
     const params = new URLSearchParams(window.location.search);
     return id = params.get('id') || 1;
+}
+
+// JSON에서 상품 ID로 특정 상품 불러오기
+async function loadProductData() {
+    try {
+        const response = await fetch('data/products.json') //JSON 파일 경로
+        const products = await response.json();
+
+        const productId = getProductIdFromURL();  // URL에서 id 값을 가져옴
+        const product = products.find(p => p.id === parseInt(productId)); // 해당 id의 상품 찾기
+
+        if (!product) {
+            console.error('해당 상품을 찾을 수 없습니다.');
+            return;
+        }
+
+        // 1. 제품명 표시
+        document.querySelector('figcaption h1').textContent = product.productName;
+
+        // 2. 스펙 표시
+        const dl = document.querySelector('figcaption dl');
+        // dl.innerHTML = ''; //초기화 필요시 추가
+        for (let [specName, specValue] of Object.entries(product.specs)) {
+            const span = document.createElement('span');
+            span.innerHTML = `<dt>${specName}:</dt> <dd>${specValue}</dd>`;
+            dl.appendChild(span);
+        }
+
+        // 3. 상품 설명 표시
+        document.querySelector('.product-description span').textContent = product.description;
+
+        // 4. 의견 표시
+        const opinionsSection = document.querySelector('.product-opinions');
+        // opinionsSection.innerHTML = ''; //초기화 필요시 추가
+        product.opinions.forEach(opinion => {
+            const article = document.createElement('article');
+            article.classList.add('opinion');
+            article.innerHTML = `
+                    <p>${opinion.content}</p>
+                    <small class="opinion-meta">
+                        작성자: ${opinion.author}
+                        <time datetime="${opinion.date}">${formatDate(opinion.date)}</time>
+                    </small>
+                `;
+            opinionsSection.appendChild(article);
+        });
+    } catch (error) {
+        console.error('데이터를 불러오는 중 오류 발생:', error);
+    }
 }
