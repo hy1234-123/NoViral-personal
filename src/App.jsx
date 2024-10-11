fc35080787c5285fd949317dabbf3bb71ee7bc8f
DKULena <32173057@dankook.ac.kr>
2024-10-11 16:47:47 +0900

    add: setUpProductListPage

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,32 @@ import Footer from './components/Footer';
 import './App1.css';  // 공통 레이아웃 스타일
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/details/:id" element={<ProductDetails />} />
-        <Route path="/discussion/:id" element={<Discussion />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
