94e9492bc819eb9e8a72ab54309edc071c61155f
이해연 <gosl664@naver.com>
2024-10-20 19:22:49 +0900

    Header.jsx 수정 (의견 조율 중)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { NavBar, SubNavBar } from './'
+import React from "react";
+import { NavBar, SubNavBar } from "./";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation(); // 현재 경로 가져오기
+
   return (
-    <header>
-      <NavBar />
-      <SubNavBar />
+    <header style={{ height: location.pathname === "/" ? "0px" : "auto" }}>
+      <NavBar /> {/* 모든 페이지에서 NavBar 렌더링 */}
+      {location.pathname !== "/" && <SubNavBar />}{" "}
+      {/* 메인 페이지가 아닌 경우에만 SubNavBar 렌더링 */}
     </header>
-  )
+  );
 }
