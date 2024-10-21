23d8e513eac0507da565bbf7f4390d5bd690e442
CONG878 <98112875+CONG878@users.noreply.github.com>
2024-10-21 13:44:01 +0900

    자료 구조 통일 및 불러오기 함수 변경
    참조 데이터 파일: products.json -> product_data.js 로 변경.
    
    UseProductData 훅 업데이트: 파일 형식 변경으로 인해 async 함수 대신 import 양식 사용.
    
    product_data.js 양식 변경:
    thumbnail 속성의 경로 양식: "./images/*' -> '/images/*"; './'으로 시작하던 경로를 '/'로 시작.
    opinions 속성 추가: 의견 객체의 배열.
    
    ProductOverview 컴포넌트 표시 형식 조정:
    .map() 메소드를 사용해 속성을 반복적으로 불러옴 -> 속성 개수만큼 직접 작성.
    유동적이지 않은 고정된 속성을 불러오고 속성 수가 많지 않으므로 단순한 방법으로 불러옴.
    
    ProductDetails 페이지 이미지 연결: 기존의 임시 이미지 대신 정보를 불러와서 표시.

--- src/hooks/UseProductData.jsx
+++ src/hooks/UseProductData.jsx
@@ -1,20 +1,12 @@
 import { useState, useEffect } from 'react';
+import productData from '../../public/data/product_data';
 
 const UseProductData = (id) => {
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
-    const fetchProductData = async () => {
-      try {
-        const response = await fetch('/data/products.json');
-        const products = await response.json();
-        const product = products.find(p => p.id === parseInt(id));
-        setProduct(product);
-      } catch (error) {
-        console.error('Error fetching product data:', error);
-      }
-    };
-    fetchProductData();
+    const product = productData.find(p => p.id === parseInt(id));
+    setProduct(product);
   }, [id]);
 
   return product;
